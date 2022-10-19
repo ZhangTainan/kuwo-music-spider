69d89b22ae55747a4e507e86cdc58d57db30bc24
zzj <2998637256@qq.com>
2022-10-19 15:31:48 +0800

    fix(spider): list can not auto clear
    When crawling the song, the mp3 of the object_ The list attribute will not be cleared

--- spider.py
+++ spider.py
@@ -22,8 +22,7 @@ class Spider:
     url_for_search = "http://www.kuwo.cn/api/www/search/searchMusicBykeyWord?key={}&pn={}&rn={}&httpsStatus=1&reqId=df4a2b91-0896-11ed-b2d2-7f9c12d55fee"
     # 获取播放地址的url
     url_for_mp3_url = "http://www.kuwo.cn/api/v1/www/music/playUrl?mid={}&type=mp3&httpsStatus=1&reqId=f0f57820-089d-11ed-a75f-6357d35c8f78"
-    # 定义一个空列表存储歌曲名和播放地址
-    mp3_list = []
+
 
     # 初始化时拼接url_for_search,接收三个参数:查询关键字,第几页,每页的歌曲数
     def __init__(self, keyword: str, page: int, num_per_page: int):
@@ -35,6 +34,8 @@ class Spider:
         res_for_search = requests.get(url=self.url_for_search, headers=self.headers).json()
         # 获取请求的歌曲信息列表
         music_list = res_for_search["data"]["list"]
+        # 定义一个空列表存储歌曲名和播放地址
+        mp3_list = []
         # 遍历列表
         for music in music_list:
             # 获取歌曲的rid用于拼接到请求歌曲播放地址的url
@@ -46,8 +47,8 @@ class Spider:
             # TODO:可能出现没有图片的情况,判断一下是不是以jpg、png结尾,这时需要把图片给个默认值
             mp3 = {"name": music["name"], "pic_url": music["pic"], "artist": music["artist"], "url": mp3_url,
                    "rid": music["rid"]}
-            self.mp3_list.append(mp3)
-        return self.mp3_list
+            mp3_list.append(mp3)
+        return mp3_list
 
 
 if __name__ == '__main__':
