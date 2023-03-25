171df36c1d446411530e145fba400846e65e792c
zzj <2998637256@qq.com>
2023-03-26 00:21:27 +0800

    feat:create front-end vue3 app

---
+++ daily-jaychou-fe/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  123
+  <SearchBox></SearchBox>
+  <Test></Test>
+</template>
+<script setup>
+import SearchBox from "./components/SearchBox.vue";
+import Test from "./components/Test.vue";
+</script>
+<style></style>
